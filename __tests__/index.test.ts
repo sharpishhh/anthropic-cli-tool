8cae70a762817a04a6d4052158ea169d3932bb76
sharpish <sarah.sharpish@gmail.com>
2026-06-07 22:18:24 -0500

    Feat: Add validateInput function and first passing tests

---
+++ __tests__/index.test.ts
@@ -0,0 +1,27 @@
+import { validateInput } from '../src';
+
+describe('validateInput', () => {
+  test('valid input returns true', () => {
+    const userInput = validateInput({ user_input: 'Cheetah' });
+    expect(userInput).toBe(true);
+  });
+
+  test('too short returns false', () => {
+    const userInput = validateInput({ user_input: 'as' });
+    expect(userInput).toBe(false);
+  });
+
+  test('too long returns false', () => {
+    const userInput = validateInput({
+      user_input: 'How do you say bathroom in Japanese?',
+    });
+    expect(userInput).toBe(false);
+  });
+
+  test('special characters return false', () => {
+    const userInput = validateInput({
+      user_input: 'There i$ no *&^( in Team!!!',
+    });
+    expect(userInput).toBe(false);
+  });
+});
